b71746d1bdf14e04c5e33ef1239d14bda1d938e6
anurag619 <anurag3rdsep@gmail.com>
2026-07-29 09:45:00 +0530

    Mark Migraine Logs as released

---
+++ src/lib/products.ts
@@ -0,0 +1,5 @@
+export const MIGRAINE_LOGS_URL = 'https://migrainelogs.com';
+export const MIGRAINE_LOGS_APP_STORE_URL =
+  'https://apps.apple.com/us/app/migraine-logs-headache-tracker/id6760580852';
+export const MIGRAINE_LOGS_GOOGLE_PLAY_URL =
+  'https://play.google.com/store/apps/details?id=com.migrainelogs.app';
